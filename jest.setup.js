0d74a4ffbc9309a37b7aac81163435beb5752a39
harapeko <gmail@harapeko.dev>
2020-06-29 02:07:26 +0900

    add 本体のテスト

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,3 @@
 const chalk = require("chalk");
 
-// ドキュメントを見る限り、こう書くのが正解ぽい
-new chalk.instance({ level: 0 });
-// chalk.level = 0;
+chalk.level = 0;
---
+++ src/__tests__/index.test.js
@@ -0,0 +1,25 @@
+const { main } = require("../");
+
+describe("toy-tree", () => {
+  let stdout;
+  let stderr;
+  const exec = (argv) => main(argv, stdout, stderr);
+
+  beforeEach(() => {
+    stdout = jest.fn();
+    stderr = jest.fn();
+  });
+
+  test("fixtures", () => {
+    const code = exec(["fixtures"]);
+
+    expect(stdout.mock.calls[0][0]).toMatchSnapshot();
+    expect(code).toBe(0);
+  });
+
+  // TODO: 本題ではないので余裕がればでOK
+  // - -Lオプションを指定した場合
+  // - ディレクトリ未指定時
+  // - 不正なディレクトリを指定した時
+  // - 不正な-Lオプションを指定した時
+});
